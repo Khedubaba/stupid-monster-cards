77d776d73012a33a6d00a28acf55a671819139b3
khedubaba <adityakhedekar49@gmail.com>
2021-12-18 22:09:35 +0530

    live search-box feature added nd UI finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -7,10 +8,12 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
-  }
 
+    // this.handleChange = this.handleChange.bind(this);
+  }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -18,11 +21,22 @@ class App extends Component {
     .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  }
 
   render() {
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter( monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()) 
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monster Rolodex</h1>
+        <SearchBox placeholder='search monsters' handleChange={e => this.setState({ searchField: e.target.value})}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
